8651c5dbd3f41ef06b6432f6d194c1e7c2f1e025
Bekov28 <orziqulov_berdiyor@mail.ru>
2026-05-12 13:30:35 +0900

    feat: created Question component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import Start from "./Start";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -18,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknown");
   }
@@ -47,7 +50,8 @@ export function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <Start numQuestions={numQuestions} />}
+        {status === "ready" && <Start numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
